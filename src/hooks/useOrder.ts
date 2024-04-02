9c53144dcdd86af97a0478a43da7d20f01218609
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-04-01 21:16:42 -0300

    agregando funcionalidad multiplicar item repetido en la ordern

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -9,7 +9,9 @@ export default function useOrder() {
     
     const itemExist = order.find(orderItem => orderItem.id === item.id)
     if(itemExist){
-        console.log('Ya existe...')
+      const updatedOrder = order.map( orderItem => orderItem.id === item.id ? {...orderItem, quantity: orderItem.quantity + 1} : orderItem )
+      
+      setOrder(updatedOrder)
     } else {
       const newItem = {...item, quantity: 1}
       setOrder([...order, newItem])
